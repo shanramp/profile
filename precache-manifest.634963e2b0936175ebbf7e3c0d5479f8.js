48f8a6b210dfe5113c60c8d78d235b3e91fb8371
shanramp <shanramp221@gmail.com>
2026-02-02 09:55:09 -0800

    build

---
+++ precache-manifest.634963e2b0936175ebbf7e3c0d5479f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8c0a04333d040fd2d0b1d67b8ec077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4dacc7586f35a0ddf11",
+    "url": "/static/css/2.2f3d5f8a.chunk.css"
+  },
+  {
+    "revision": "883867f7219aae88a4dd",
+    "url": "/static/css/main.26a36eb5.chunk.css"
+  },
+  {
+    "revision": "c4dacc7586f35a0ddf11",
+    "url": "/static/js/2.70a17281.chunk.js"
+  },
+  {
+    "revision": "d3e49d62c5e83bd760c7639e00b15fb0",
+    "url": "/static/js/2.70a17281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45dbd4cf73473334917",
+    "url": "/static/js/3.e6429ce8.chunk.js"
+  },
+  {
+    "revision": "883867f7219aae88a4dd",
+    "url": "/static/js/main.95712ea2.chunk.js"
+  },
+  {
+    "revision": "ca49d75fd452eaca4098",
+    "url": "/static/js/runtime~main.cf3a1495.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
